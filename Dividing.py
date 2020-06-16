2a9910fb21140020f067605b8b31c5a7f51ae10e
minji-OH <63227680+minji-OH@users.noreply.github.com>
2020-06-16 11:33:23 +0900

    Delete Dividing.py

--- Dividing.py
+++
@@ -1,9 +0,0 @@
-def solution(arr, divisor):
-    answer = []
-    for i in arr:
-        if i % divisor == 0:
-            answer.append(i)
-    if len(answer) == 0:
-        return [-1]
-    else:
-        return sorted(answer)
